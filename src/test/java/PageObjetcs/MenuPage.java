7d02348800bb84f32b6653a460e180baab49f10d
Joel <joel28leiva@gmail.com>
2021-12-06 21:01:42 -0500

    se creo mi primer metodo en MenuPage

--- src/test/java/PageObjetcs/MenuPage.java
+++ src/test/java/PageObjetcs/MenuPage.java
@@ -1,4 +1,37 @@
 package PageObjetcs;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class MenuPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(linkText="Generate Card Number") private WebElement lnk_generarTarjeta;
+
+
+    //constructor
+    public MenuPage(WebDriver d) {
+        //inicializar as variables
+        driver=d;
+        //esperar algo para utilizar
+        //la espera explicita solo te espera hasta encontra el objeto
+        // la espera implicita
+        wait=new WebDriverWait(driver,30);
+        PageFactory.initElements(driver,this);
+
+    }
+
+    public void clickGenerarTarjeta(){
+        //Espera que cargue la pagina con el wait
+        wait.until(ExpectedConditions.elementToBeClickable(lnk_generarTarjeta));
+        lnk_generarTarjeta.click();
+
+    }
 }
